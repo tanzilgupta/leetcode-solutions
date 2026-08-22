70b3597556c0fbf901646d5814eec708341cd864
Tanzil <tanzil2gupta@gmail.com>
2026-08-22 14:57:20 +0530

    [3622] [Time Beats: 100.00%] [Memory Beats: 79.33%] - LeetPush

---
+++ 3622-Check-Divisibility-by-Digit-Sum-and-Product.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum = 0;
+        int prod = 1;
+        int temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit;
+            prod *= digit;
+            temp /= 10;
+        }
+        int total = sum + prod;
+        return total != 0 && n % total == 0;
+    }
+}
