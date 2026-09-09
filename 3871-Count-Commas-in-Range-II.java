691de949e37c1d04a47dcb28c2141131d4f0414e
Tanzil <tanzil2gupta@gmail.com>
2026-09-09 13:54:45 +0530

    [3871] [Time Beats: 99.36%] [Memory Beats: 71.15%] - LeetPush

---
+++ 3871-Count-Commas-in-Range-II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+
+        for (long p = 1000; p <= n; p *= 1000) {
+            ans += n - p + 1;
+
+            if (p > n / 1000) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
