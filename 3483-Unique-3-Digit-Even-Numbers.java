5f4abe147eb7ce5c66db04189f7cad9cf4c368b3
Tanzil <tanzil2gupta@gmail.com>
2026-09-11 22:42:21 +0530

    [3483] [Time Beats: 24.00%] [Memory Beats: 7.47%] - LeetPush

---
+++ 3483-Unique-3-Digit-Even-Numbers.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] freq = new int[10];
+
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        int count = 0;
+
+        for (int num = 100; num <= 998; num += 2) {
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
+
+            int[] need = new int[10];
+            need[a]++;
+            need[b]++;
+            need[c]++;
+
+            boolean possible = true;
+
+            for (int d = 0; d <= 9; d++) {
+                if (need[d] > freq[d]) {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
